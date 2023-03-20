d760521a6d52bfa2d8e01855ae655cae21f8009d
deoncarlette <104539455+deandrawhitby@users.noreply.github.com>
2023-03-20 20:49:07 +0000

    Period 5 tips

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 def hangman():
   ...
+  
+  # 1. Create a wordbank with a list of strings
+
+  # 2. Randomly select a word from the word bank for each new game
+
+  # 3. Optionally - Create an object with underscores to display the letters of the word
+
+  # 4. Ask the user to guess a letter
+
+  # 5. Check if the letter guessed is in the word
+
+  # 6. Create rules for if guess is incorrect
+
+  # 7. Create rules for if the guess is correct
+
+
+
+
+  
