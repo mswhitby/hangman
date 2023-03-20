c34a0378571830c8ac848261cbb642f9f6cf0fc1
deoncarlette <104539455+deandrawhitby@users.noreply.github.com>
2023-03-20 17:33:28 +0000

    Added starter tips

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 def hangman():
-  ...
+  
+  # 1. Create a wordbank using a list of strings
+  wordbank = []
+
+  # 2. Randomly select a word from the wordbank for each new game.
+  # Look up "list methods" to see how to randomly select an item from a list
+  word = ...
+
+
+  # 3. Ask the user to guess a letter.
+  # Look up how to get an input from a user
+  user_guess = ...
+
+
+  # 4. Check to see if the letter is in the word
+  # Look up string methods
+  is_correct = ...
+  
+
+  
