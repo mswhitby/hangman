b54caf9f47587b5b3c0cb3eff132478f91b83aa3
deoncarlette <104539455+deandrawhitby@users.noreply.github.com>
2023-03-20 20:59:06 +0000

    updated readme.md

--- main.py
+++ main.py
@@ -5,23 +5,15 @@ def hangman():
 
   # 2. Randomly select a word from your wordbank
   # Hint: Figure out how to randomly select in element of a list in python
- 
 
   # 3. Limit the number of guesses
   # Hint: Create a variable to count the number of guesses
 
-
   # 4. Ask the user to guess a letter
   # Hint: Use the input function, and include instructions for the user
 
-  
   # 5. Check if the user's letter is in the word
-
 
   # 6. Create rules for if the user guesses correctly
 
-
   # 7. Create rules for if the user guesses incorrectly
-
-  
-
