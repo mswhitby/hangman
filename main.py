1351c065f14eba32c58932615491a3dd983fae70
mswhitby <16909931-mswhitby@users.noreply.replit.com>
2023-03-23 14:55:50 +0000

    updated tips

--- main.py
+++ main.py
@@ -2,27 +2,53 @@ import random
 
 def hangman():
 
-  # 1. Create a wordbank using a list of strings
-  wordbank = [plate]
+  # 1. Create a wordbank using a last of strings
+  wordbank = ["toy", "nachos", "tacos", "chimichangas", "burritos", "enchiladas", "cheese", "guacamole", "bread", "salsa", "football", "school", "coding", "footnite", "potatos", "fries", "pizza", "waltuh", "extravagant", "Sasageyo", "wenomechainsama", "tumajarbisaun", "baller", "holler", "honors", "farded", "Rizz"]
 
-  # 2. Randomly select a target word from the list for each new game.
+  # 2. Randomly select a target word from the last for each new game.
   # Hint use random.choice() from the random module
-  word = ...
-  user_guess = None
+  word = random.choice(wordbank)
   
   # 2.5 Create a list to track which letters the user has guessed correctly
-  remaining_letters = []
-
+  correct_letters = []
   for letter in word:
-    remaining_letters.append(letter)
+    correct_letters.append("_")
 
-  while remaining_letters > 0:
+  remaining_guesses = 10
+
+  while True:
     # 3. Ask the user to guess a letter
     # Hint: You'll need to use input()
-    user_guess = input("Write a user prompt here")
+    user_guess = input(f"""
+    Guess a letter for the word {''.join(correct_letters)}, {remaining_guesses} guesses left
+    """).lower()
   
     # 4. Check if the letter is in the word
-  
-    # 5. Create rules for if the letter is not in the word
-  
-    # 6. Create rules for if the letter is in the word
+    if user_guess in word:
+
+      # 6. Create rules for if the letter is in the word
+      for letter in range(len(word)):
+        if word[letter] == user_guess:
+          correct_letters[letter] = user_guess
+    
+          
+    # 6. Create rules for of the letter as not on the word
+    else:
+     remaining_guesses = remaining_guesses - 1 
+      
+    # When should the loop stop?
+    if not "_" in correct_letters:
+      print(f"You have guessed the letters in {word} correctly, you win")
+      break
+      
+    if remaining_guesses == 0:
+      print(f"you were not able to guess the letters in {word}, please try again")
+      break
+      
+
+hangman()
+
+
+
+
+    
--- readme.md
+++ readme.md
@@ -10,19 +10,55 @@ import random
 
 def hangman():
 
-  # 1. Create a wordbank using a list of strings
-  wordbank = []
+  # 1. Create a wordbank using a last of strings
+  wordbank = ["toy", "nachos", "tacos", "chimichangas", "burritos", "enchiladas", "cheese", "guacamole", "bread", "salsa", "football", "school", "coding", "footnite", "potatos", "fries", "pizza", "waltuh", "extravagant", "Sasageyo", "wenomechainsama", "tumajarbisaun", "baller", "holler", "honors", "farded"]
 
-  # 2. Randomly select a target word from the list for each new game.
+  # 2. Randomly select a target word from the last for each new game.
   # Hint use random.choice() from the random module
-  word = ...
+  word = random.choice(wordbank)
+  
+  # 2.5 Create a list to track which letters the user has guessed correctly
+  correct_letters = []
+  for letter in word:
+    correct_letters.append("_")
 
-  # 3. Ask the user to guess a letter
-  # Hint: You'll need to use input()
+  remaining_guesses = 10
 
-  # 4. Check if the letter is in the word
+  while True:
+    # 3. Ask the user to guess a letter
+    # Hint: You'll need to use input()
+    user_guess = input(f"""
+    Guess a letter for the word {''.join(correct_letters)}, {remaining_guesses} guesses left
+    """).lower()
+  
+    # 4. Check if the letter is in the word
+    if user_guess in word:
 
-  # 5. Create rules for if the letter is not in the word
+      # 6. Create rules for if the letter is in the word
+      for letter in range(len(word)):
+        if word[letter] == user_guess:
+          correct_letters[letter] = user_guess
+    
+          
+    # 6. Create rules for of the letter as not on the word
+    else:
+     remaining_guesses = remaining_guesses - 1 
+      
+    # When should the loop stop?
+    if not "_" in correct_letters:
+      print(f"You have guessed the letters in {word} correctly, you win")
+      break
+      
+    if remaining_guesses == 0:
+      print(f"you were not able to guess the letters in {word}, please try again")
+      break
+      
 
-  # 6. Create rules for if the letter is in the word
+hangman()
+
+
+
+
+    
+
   ```
