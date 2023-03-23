41ee2071c4094a8c8f88335198c782b7410a59a8
Deandra Whitby <63321092+deoncarlette@users.noreply.github.com>
2023-03-23 19:22:00 +0000

    updated code

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-def hangman():
+import random
 
-  # 1. Create a list of strings that will comprise your wordbank.
-  wordbank = []
+wordbank = [
+  "apples", "banana", "orange", "mango", "pineapple", "kiwi", "strawberry",
+  "watermelon", "grapes", "blueberries"
+]
 
-  # 2. Randomly select a word from your wordbank
-  # Hint: Figure out how to randomly select in element of a list in python
 
-  # 3. Limit the number of guesses
-  # Hint: Create a variable to count the number of guesses
+def hangman(remaining_guess=6):
+  word = random.choice(wordbank)
 
-  # 4. Ask the user to guess a letter
-  # Hint: Use the input function, and include instructions for the user
+  # remaining_guess = 6
 
-  # 5. Check if the user's letter is in the word
+  guessed_letters = ["_" for letter in word]
 
-  # 6. Create rules for if the user guesses correctly
+  # guessed_letters = []
+  # for letter in word:
+  #   guessed_letters.append("_")
 
-  # 7. Create rules for if the user guesses incorrectly
+  while True:
+    print(" ".join(guessed_letters))
+    print("Remaining guesses: {}".format(remaining_guess))
+
+    guess = input("Enter a letter: ").lower()
+
+    if guess in word:
+      for i in range(len(word)):
+        if word[i] == guess:
+          guessed_letters[i] = guess
+    else:
+      remaining_guess -= 1
+
+    if "_" not in guessed_letters:
+      print("congratulations! You won The word is {}.".format(word))
+      break
+    elif remaining_guess == 0:
+      print("Sorry, you lost. The word was {}.".format(word))
+      break
+
+
+hangman()
