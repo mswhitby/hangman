4b363e039a70561b6f58b57a12c40e9791a694b1
mswhitby <16909931-mswhitby@users.noreply.replit.com>
2023-03-23 14:54:48 +0000

    Updated class tips

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import random
 def hangman():
 
   # 1. Create a wordbank using a list of strings
-  wordbank = []
+  wordbank = [plate]
 
   # 2. Randomly select a target word from the list for each new game.
   # Hint use random.choice() from the random module
   word = ...
+  user_guess = None
+  
+  # 2.5 Create a list to track which letters the user has guessed correctly
+  remaining_letters = []
 
-  # 3. Ask the user to guess a letter
-  # Hint: You'll need to use input()
+  for letter in word:
+    remaining_letters.append(letter)
 
-  # 4. Check if the letter is in the word
-
-  # 5. Create rules for if the letter is not in the word
-
-  # 6. Create rules for if the letter is in the word
+  while remaining_letters > 0:
+    # 3. Ask the user to guess a letter
+    # Hint: You'll need to use input()
+    user_guess = input("Write a user prompt here")
+  
+    # 4. Check if the letter is in the word
+  
+    # 5. Create rules for if the letter is not in the word
+  
+    # 6. Create rules for if the letter is in the word
